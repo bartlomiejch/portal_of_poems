76564e7c208dac0f17e33f050b2a8b508c79fab4
Bartlomiej Chowaniec <bartek.chowaniec@gmail.com>
2014-07-15 14:57:33 +0200

    Update views.py

--- views.py
+++ views.py
@@ -1,67 +1,93 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 from django.http import HttpResponseRedirect
+from django.contrib.sessions.models import Session
+from django.contrib import auth
+
 
 from poems.models import Poem, Comment, Stropha
-from poems.forms import PoemForm, CommentForm, StrophaAddForm
+
 
 def poems(request):
     return render_to_response('poems/poems.html', {'poems':Poem.objects.all().order_by('like').reverse()})	
-
 
 def poem(request, dupa = 1):
 	return render_to_response('poems/poem.html', {'poem':Poem.objects.get(id = dupa) })
 
 
-def comment(request, poem_id, comment_id):
-	if request.POST:
-		form = CommentForm(request.POST)
-		if form.is_valid():
-			form.save()
 
-			return HttpResponseRedirect('poems')
-	return HttpResponseRedirect('poems/poem.html',{'comment':Comment.objects.get(id = comment_id)})
-
+def comment(request, poem_id):
+	if poem_id:
+		if request.method == 'POST':
+			title = request.POST.get('title')
+			text_of_comment = request.POST.get('text_of_comment')
+			nick = request.POST.get('nick')
+			comment = Comment.objects.create(title = title, text = text_of_comment, nick = nick, poem = poem_id)
+			comment.save()
+			comments = Poem.objects.get(id = poem_id)
+			all_comments = comments.poem_set.all()
+			args = {}
+			args.update(csrf(request))
+			args['all_comments'] = all_comments
+			return HttpResponseRedirect('/poems/get/%s' % poem_id, args)
+		else:
+			comments = Poem.objects.get(id = poem_id)
+			all_comments = comments.poem_set.all()
+			args = {}
+			args.update(csrf(request))
+			args['all_comments'] = all_comments
+			return HttpResponseRedirect('/poems/get/%s' % poem_id, args)
+			
 
 def create(request):
-	if request.POST:
-		form = PoemForm(request.POST)
-		if form.is_valid():
-			form.save()
-		
-			return HttpResponseRedirect('/poems/all')	
+	if request.method == 'POST':
+		a = request.POST.get('poems_title')
+		b = request.POST.get('poems_text')
+		if request.user.is_authenticated():
+			if a and b is not None:
+				di = {}
+				di.update(csrf(request))
+				poem = Poem.objects.create(title = a, text = b)
+				poem.save()
+				return HttpResponseRedirect('/poems/all', di)
+			else:
+				message = 'Oba pola muszą być wypełnione'
+				args = {}
+				args['message'] = message
+				args.update(csrf(request))
+				return render_to_response('poems/create.html', args)
+		else:
+			message_to_not_user = 'Wiersze mogą dodawać tylko zalogowani użytkownicy - '
+			arg = {}
+			arg['message_to_not_user'] = message_to_not_user
+			arg.update(csrf(request))
+			return render_to_response('poems/create.html', arg)
+			
 	else:
-		form = PoemForm()
-
-	args = {}
-	args.update(csrf(request))
-	args['form'] = form
-	return render_to_response('poems/create.html', args)
+		ar = {}
+		ar.update(csrf(request))
+		return render_to_response('poems/create.html', ar)
 
 
 def like_poem(request, poem_id):
-    if poem_id:
-        a = Poem.objects.get(id = poem_id)
-        count = a.like
-        count += 1
-        a.like = count
-        a.save()
-        return HttpResponseRedirect('/poems/get/%s' % poem_id)
+	if poem_id:
+		a = Poem.objects.get(id = poem_id)
+		count = a.like
+		count += 1
+		a.like = count
+		a.save()
+		return HttpResponseRedirect('/poems/get/%s' % poem_id)
+		
+
 
 
 def social_poem(request):
 	if request.method == "POST":
-		form = StrophaAddForm(request.POST)
-		if form.is_valid():
-			my_model = Stropha()
-			my_model.stropha = form.cleaned_data['add_stropha']
-			my_model.save()
+		a = request.POST.get('strofa')
+		nowa_strofa = Stropha.objects.create(stropha = a)
 
-	form = StrophaAddForm()
 	poetry = Stropha.objects.all()
-	c ={'poetry': poetry, 'form':form}
+	c ={'poetry': poetry}
 	c.update(csrf(request))
 	return render_to_response('poems/social_poem.html', c)	    
-
-
-			
