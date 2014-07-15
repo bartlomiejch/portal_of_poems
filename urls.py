11ff2616b4dd74ba70c5098ee74682723aba3f9a
Bartlomiej Chowaniec <bartek.chowaniec@gmail.com>
2014-07-15 12:53:08 +0200

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+	url(r'^all/$', 'poems.views.poems'),
+	url(r'^social_poem/$', 'poems.views.social_poem'),
+	url(r'^get/(?P<dupa>\d+)/$', 'poems.views.poem'),
+	url(r'^create/$', 'poems.views.create'),
+	url(r'^like/(?P<poem_id>\d+)/$', 'poems.views.like_poem'),
+	url(r'^comment/(?P<poem_id>\d+)/$', 'poems.views.comment'),
+	
+    url(r'^admin/', include(admin.site.urls)),
+)
