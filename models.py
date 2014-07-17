4889313f335c846457a07600b81379a1a269e285
Bartlomiej Chowaniec <bartek.chowaniec@gmail.com>
2014-07-17 17:26:11 +0200

    Update models.py

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Poem(models.Model):
 	title = models.CharField(max_length = 200)
 	text = models.TextField()
-	pub_date = models.DateTimeField(auto_now_add = True)
 	like = models.IntegerField(default = 0)
 
 
@@ -14,7 +13,6 @@ class Poem(models.Model):
 class Comment(models.Model):
 	title = models.CharField(max_length = 200)
 	text = models.TextField()
-	pub_date = models.DateTimeField(auto_now_add = True)
 	nick = models.CharField(max_length= 100)
 	poem = models.ForeignKey(Poem, null=True)
 
