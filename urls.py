2069f0a3b0bfec46dd49717a1ebd5f02c1c6d09f
Bartlomiej Chowaniec <bartek.chowaniec@gmail.com>
2014-07-18 13:53:02 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^all/$', 'poems.views.poems'),
 	url(r'^social_poem/$', 'poems.views.social_poem'),
-	url(r'^get/(?P<dupa>\d+)/$', 'poems.views.poem'),
+	url(r'^get/(?P<poem_id>\d+)/$', 'poems.views.poem'),
 	url(r'^create/$', 'poems.views.create'),
 	url(r'^like/(?P<poem_id>\d+)/$', 'poems.views.like_poem'),
 	url(r'^comment/(?P<poem_id>\d+)/$', 'poems.views.comment'),
